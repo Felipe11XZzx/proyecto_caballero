bfc71837b63a24833bd5e4a215aeb808191e7156
Felipe11XZzx <153298467+Felipe11XZzx@users.noreply.github.com>
2025-02-04 16:17:47 +0100

    Falta implementar barra de vida del Golem

--- golem.gd
+++ golem.gd
@@ -6,11 +6,7 @@ const JUMP_VELOCITY = -400.0
 const projectile_scene = preload("res://arm_projectile.tscn")
 
 func _ready() -> void:
-	$movible/AnimatedSprite2D.play("throw_animation")
-	var projectile = projectile_scene.instantiate()
-	add_child(projectile)
-	projectile.position.x = $movible/projectile_position.position.x
-	projectile.position.y = $movible/projectile_position.position.y
+	pass
 	
 func _process(delta: float) -> void:
 	pass
@@ -18,3 +14,30 @@ func _process(delta: float) -> void:
 
 func _physics_process(delta: float) -> void:
 	pass
+	
+func shot_projectile():
+	$movible/Golem_sprite.play("throw_animation")
+	var projectile = projectile_scene.instantiate()
+	projectile.position.x = $movible/projectile_position.position.x
+	projectile.position.y = $movible/projectile_position.position.y
+	add_child(projectile)
+
+
+func shot_laser():
+	pass
+	
+func _on_timer_timeout() -> void:
+	var rng = RandomNumberGenerator.new()
+	var number = rng.randi_range(1, 3)
+	
+	if (number == 1):
+		$AnimationPlayer.play("shoot_laser_projectile")
+	
+	if (number == 2):
+		$AnimationPlayer.play("meele_animation")
+		
+	if (number == 3):
+		$AnimationPlayer.play("shoot_projectile_animation")
+		
+	$Timer.wait_time = number
+	$Timer.start()
--- knigth.gd
+++ knigth.gd
@@ -23,3 +23,8 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()"""
+
+
+func _on_knigtht_hurtbox_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	if area.is_in_group("enemies"):
+		$CanvasLayer/Vida.get_damage(25)
