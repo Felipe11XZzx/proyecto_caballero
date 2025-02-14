f41a8b9e7605f8e630247a086c05472287f11c3e
Felipe11XZzx <153298467+Felipe11XZzx@users.noreply.github.com>
2025-02-14 16:25:29 +0100

    Implementando logica sprite esqueleto

---
+++ skeleton.gd
@@ -0,0 +1,76 @@
+extends CharacterBody2D
+
+@onready var skeleton: CharacterBody2D = $"."
+#@export var is_laser_finished = false
+#@export var is_attacking = false
+var activate = false
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+#const projectile_scene = preload("res://arm_projectile.tscn")
+
+func _ready() -> void:
+	#$movible/laser/laser_sprite.visible = false
+	pass
+	
+func _process(delta: float) -> void:
+	var is_playing = $AnimationPlayer.is_playing()
+	$AnimationPlayer.play("walk_skeleton_animation")
+	skeleton.velocity.x = direction * SPEED
+		
+	if skeleton.velocity.x < 0:
+		$"movible".scale.x = -abs($"movible".scale.x)
+		skeleton.move_and_slide()
+						
+	if skeleton.velocity.x > 0:
+		$"movible".scale.x = abs($"movible".scale.x)
+		skeleton.move_and_slide()				
+	#$AnimationPlayer.play("attack_skeleton_animation")
+	pass
+
+func _physics_process(delta: float) -> void:
+	pass
+	
+#func shot_projectile():
+	#$movible/Golem_sprite.play("throw_animation")
+	#var projectile = projectile_scene.instantiate()
+	#projectile.position.x = $movible/projectile_position.position.x
+	#projectile.position.y = $movible/projectile_position.position.y
+	#add_child(projectile)
+
+
+#func shot_laser():
+	#pass
+	
+func _on_timer_timeout() -> void:
+	var rng = RandomNumberGenerator.new()
+	var number = rng.randi_range(1, 3)
+	#var is_playing = $AnimationPlayer.is_playing()
+	#$movible/laser/laser_sprite.visible = false
+	#print("Animacion Aleatoria Numero: " + str(number))
+	#is_attacking = true
+	"""
+	if activate:
+		if number == 1:
+			#$movible/laser/laser_sprite.visible = true
+			$AnimationPlayer.play("attack_skeleton_animation")
+			print("Entra en la animacion de ataque esqueleto")
+			print("Sale de la animacion del ataque esqueleto\n")
+	
+		elif number == 2:
+			print("Entra en la animacion del escudo")
+			$AnimationPlayer.play("shield_skeleton_animation")
+			print("Sale de la animacion del escudo \n")
+
+		elif number == 3:
+			print("Entra en la animacion de caminar")
+			$AnimationPlayer.play("walk_skeleton_animation")
+			print("Sale de la animacion de caminar\n")
+		
+	await $AnimationPlayer.animation_finished
+	#is_attacking = false
+	
+	var restartTime = rng.randi_range(2, 3)
+	$Timer.wait_time = restartTime
+	$Timer.start()
+	"""
+	
